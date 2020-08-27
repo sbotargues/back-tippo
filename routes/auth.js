07a1b46583d21e1d04b4b61fd8ff9d93ca2a4209
Sergi Botargues <sergibotargues@MacBook-Air-de-Sergi.local>
2020-08-27 09:52:42 +0200

    jueves deploy heroku....

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const createError = require("http-errors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 const User = require("../models/user");
 
