7b6cf237324024b605b8e0efeb3445e85b0a10c6
Sergi Botargues <sergibotargues@MacBook-Air-de-Sergi.local>
2020-08-27 13:10:53 +0200

    jueves deploy heroku FINAL

--- routes/file-upload-routes.js
+++ routes/file-upload-routes.js
@@ -16,4 +16,16 @@ router.post("/upload", uploader.single("postPhoto"), (req, res, next) => {
   res.json({ secure_url: req.file.secure_url });
 });
 
+router.post("/uploadProfile", uploader.single("profilePhoto"), (req, res, next) => {
+  // console.log('file is: ', req.file)
+
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // get secure_url from the file object and save it in the
+  // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
+  res.json({ secure_url: req.file.secure_url });
+});
+
 module.exports = router;
